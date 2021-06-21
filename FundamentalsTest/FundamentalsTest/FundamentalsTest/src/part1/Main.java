c75390da7d81ffdcab82e38e51ad9682bbb29276
Nani <83227319+NaniNasser@users.noreply.github.com>
2021-06-21 17:50:48 +0200

    Add files via upload

---
+++ FundamentalsTest/FundamentalsTest/FundamentalsTest/src/part1/Main.java
@@ -0,0 +1,8 @@
+package part1;
+
+public class Main {
+    public static void main(String[] args) {
+HotDrink hotDrink = new HotDrink(12,62,"Temp");
+ColdDrink coldDrink = new ColdDrink("test",77);
+    }
+}
