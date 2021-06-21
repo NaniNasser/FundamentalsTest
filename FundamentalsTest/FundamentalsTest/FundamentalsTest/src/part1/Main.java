2e072a447e6d639e01263bd03aa89fcf46c2ecc7
Nani <83227319+NaniNasser@users.noreply.github.com>
2021-06-21 17:51:25 +0200

    Delete FundamentalsTest directory

--- FundamentalsTest/FundamentalsTest/FundamentalsTest/src/part1/Main.java
+++
@@ -1,8 +0,0 @@
-package part1;
-
-public class Main {
-    public static void main(String[] args) {
-HotDrink hotDrink = new HotDrink(12,62,"Temp");
-ColdDrink coldDrink = new ColdDrink("test",77);
-    }
-}
--- FundamentalsTest/FundamentalsTest/FundamentalsTest/src/part3/Main.java
+++
@@ -1,7 +0,0 @@
-package part3;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
